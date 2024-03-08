083c47170e97d54f5d3e48bf8ba1d760ef389e6b
Jeffrey Karikari <jeffreyk31k@gmail.com>
2024-03-07 17:53:18 -0800

    task 2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,13 +15,11 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user_agent = 'android:com.example.myredditapp:v1.2.3 (by /u/kemitche)'
+    headers = {
+            "user_agent": 'linux:0x16.api.advanced:v1.2.3 (by /u/kemitche)'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
-
-    try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
+    response = get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get('data')
+    return results.get('subscribers')
